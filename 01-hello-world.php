54e797e8d427557cd1427c4449b05c4be0563726
Thiagowski <thiagopereira3@hotmail.com>
2024-10-15 16:28:15 -0300

    usando a tag "echo"

--- 01-hello-world.php
+++ 01-hello-world.php
@@ -11,8 +11,12 @@
 
     <!-- Delimitador de abertura -->
     <?php
-
+    echo "Olá!";
+    echo "<p>Programador Web</p>";
+    echo "<p style='color:red'>Senac Penha</p>";
+    echo "<p style=\"color:red\">Senac Penha</p>";
     ?>
+<!-- Delimitador de fechamento -->
 
 </body>
 </html>
